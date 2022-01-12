64e9ace285a7e29bf3665ad517389ace67d8c111
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2022-01-12 00:15:00 -0300

    Scrape next page link requirement done

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    css_string = "div.tec--list.tec--list--lg > a ::attr(href)"
+    return selector.css(css_string).get()
 
 
 # Requisito 4
