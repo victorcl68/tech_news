4de5963725ed72bc2c12a786d0e6a9ca608719c3
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2022-01-13 02:31:36 -0300

    Import libraries and tenth requirement done

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,24 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+
+    news.sort(
+        key=lambda x: x["shares_count"] + x["comments_count"], reverse=True
+    )
+    # https://www.w3schools.com/python/ref_list_sort.asp
+
+    top_5_news_list = news[:5]
+
+    news_list = []
+    for each_news in top_5_news_list:
+        title_tuple = each_news["title"]
+        url_tuple = each_news["url"]
+        news_list.append((title_tuple, url_tuple))
+
+    return news_list
 
 
 # Requisito 11
